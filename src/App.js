6c73c8dc25dccbfb5c1bec008e5caeeaba15e085
aleksandarpan <75281813+aleksandarpan@users.noreply.github.com>
2022-09-13 14:33:48 +0200

    Update App.js
    dodadeno tekst

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there! by Aleksandar Panchevski</p>
+        <p>Bye there! by Aleksandar Panchevski </p>
         <a
           className="App-link"
           href="https://reactjs.org"
