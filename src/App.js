fe8e5ca83e564828d35ebe511b26abc78abe7359
aleksandarpan <75281813+aleksandarpan@users.noreply.github.com>
2022-09-14 10:43:29 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React so Aleksandar Panchevski
         </a>
       </header>
     </div>
