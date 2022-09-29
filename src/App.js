33988a20138bf2c5821d19c03d765b0fdec83d0a
aleksandarpan <75281813+aleksandarpan@users.noreply.github.com>
2022-09-29 15:20:02 +0200

    ke uspeam.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there! by Aleksandar Panchevski ke uspeam.  </p>
+        <p>Bye there! by Aleksandar Panchevski ke uspeam. 
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
