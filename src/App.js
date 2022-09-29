db952126dac858c076390dfd1cb7cec9706641fa
aleksandarpan <75281813+aleksandarpan@users.noreply.github.com>
2022-09-29 16:37:33 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Bye there! by Aleksandar Panchevski ke uspeam. 
+        <p>Bye there! by AleksandarPanchevski ke uspeam. 
         </p>
         <a
           className="App-link"
